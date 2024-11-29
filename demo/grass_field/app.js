b8da936f4f39ec466c895a0fc0eeb6d4c815b577
Blu <90506743+BlueG15@users.noreply.github.com>
2024-11-30 00:53:47 +0700

    optimization update
    uses KD tree with d = 2 now for fast gras fetching, supports up to 2000 blades of glass but beyond 1400 it looks not great

--- demo/grass_field/app.js
+++ demo/grass_field/app.js
@@ -1,63 +1,81 @@
-const grassNumber = 580
-const affectRadius = 100
-var grassArr = []
+const padding_w = 0;
+const padding_h = 20;
 
-var prevMx = 0
-var prevMy = 0
+const width = window.visualViewport.width - padding_w
+const height = window.visualViewport.height - padding_h
+
+const grassNumber = 1400
+const numberOfGrass = 40//Math.floor((Math.PI * affectRadius * affectRadius) / (width * height));
+
+let grassMap = new Map();
+let grassArr = [];
+let grassTree = undefined;
 
 function init(){
-    const width = window.visualViewport.width
-    const height = window.visualViewport.height
-
     var a = ""
+
     for(var i = 0; i < grassNumber; i++){
-        var x = rng(width + 25, -1, false)
-        var y = rng(height + 25, -1, false)
+        var x = rng(width, padding_w, false)
+        var y = rng(height, padding_h, false)
         var id = `${x}:${y}`
-        grassArr.push(id)
-        a += `<div id = "${id}" style = "top: ${y}px; left: ${x}px" class = "grass"></div>`
+        grassArr.push([x, y])
+
+        let t = "";
+        let k = rng(0, 2, true);
+        //if(k) t = " anim";
+
+        a += `<div id = "${id}" style = "top: ${y}px; left: ${x}px" class = "grass${t}"></div>`
     }
 
+    grassTree = new KDTree(2, grassArr);
     document.getElementById("field").innerHTML = a
+    
+    grassArr.forEach((i, index) => grassMap[`${i[0]}:${i[1]}`] = { target : document.getElementById(`${i[0]}:${i[1]}`), timeoutID : undefined })
+    grassArr = [];
 
     var grassList = document.getElementsByClassName("grass")
     for(var i = 0; i < grassList.length; i++){
         var rot = rng(30, -30, false)
         grassList[i].style.setProperty('--rot', rot + 'deg');
-        grassList[i].style.setProperty('--rot2', rot + 20 + 'deg');
-        grassList[i].style.setProperty('--rot3', rot - 25 + 'deg');
+        grassList[i].style.setProperty('--rot2', rot + 30 + rng(10, 0) + 'deg');
+        grassList[i].style.setProperty('--rot3', rot - 35 - rng(10, 0) + 'deg');
+        //grassList[i].style.setProperty('--delay', rng(5, 0, true) + 's');
     }
 
     window.onmousemove = (event) => {
-        var grassList2 = grassArr.slice().filter(i => {
-            var x = i.split(':')[0]
-            var y = i.split(':')[1]
-
-            var distance = Math.abs(event.x - x) + Math.abs(event.y - y);
-            if(distance > affectRadius) return false;
-            else return true
-        })
-
+        if(event.movementX == 0) return;
+        var grassList2 = grassTree.findKNearestNeighbor([event.x, event.y], numberOfGrass, (p1, p2) => {
+            //return p1.reduce((sum, val, i) => sum + (val - p2[i]) ** 2, 0);
+            //return Math.max(Math.abs(p1[0] - p2[0]), Math.abs(p1[1] - p2[1]))
+            return (event.movementX < 0) ? Math.abs(p2[0] * p2[1] - p1[1] * p1[0]) : Math.abs(p1[0] * p1[1] - p2[1] * p2[0])
+        }).map(i => `${i.point[0]}:${i.point[1]}`);
+        
         grassList2.forEach(i => {
-            if(event.x < prevMx) {
+            if(event.movementX < 0) {
                 //let r = rng(100, 0, false);
                 var a = 'rotLeft';
             } else {
                 //let r = rng(100, 0, false);
                 var a = 'rotRight';
             }
-            document.getElementById(i).classList.add(a)
-            setTimeout((i) => {
+            //grassMap[i].target.classList.remove("anim");
+            grassMap[i].target.classList.add(a);
+            if(grassMap[i].timeoutID) clearTimeout(grassMap[i])
+            let timeoutID = setTimeout(() => {
                 try{
-                    document.getElementById(i).classList.remove("rotRight")
+                    grassMap[i].target.classList.remove("rotRight")
                 }catch(err){}
                 try{
-                    document.getElementById(i).classList.remove("rotLeft")
+                    grassMap[i].target.classList.remove("rotLeft")
                 }catch(err){}
-            }, 900, i)
+                grassMap[i].timeoutID = undefined
+
+                // let k = rng(0, 2, true);
+                // if(k) grassMap[i].target.classList.add("anim");
+
+            }, rng(900, 300, true))
+            grassMap[i].timeoutID = timeoutID;
         })
-        prevMx = event.x
-        prevMy = event.y
     }
 }
 
